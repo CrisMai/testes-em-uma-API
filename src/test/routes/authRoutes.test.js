fa169d77192293243ace2ac120903053391bbe11
unknown <crismai.dev@gmail.com>
2023-10-05 17:15:21 -0300

    Testando rota de login

---
+++ src/test/routes/authRoutes.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { afterEach, beforeEach, describe } from '@jest/globals';
+import app from '../../app';
+
+let servidor;
+beforeEach(() => {
+  const porta = 3000;
+  servidor = app.listen(porta);
+});
+
+afterEach(() => {
+  servidor.close();
+});
+
+//Código omitido
+
+describe('Testando a rota login (POST)', () => {
+  it('O login deve possuir um email e senha para se autenticar', async () => {
+    const loginMock = {
+      email: 'cris@teste.com.br',
+    };
+  
+    await request(servidor)
+      .post('/login')
+      .send(loginMock)
+      .expect(500)
+      .expect('"A senha de usuario é obrigatório."');
+  });
+
+});
+
+
