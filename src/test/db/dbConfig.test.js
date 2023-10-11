0f2de750b2738ea95a52b07b4caecb8f1ad675b7
unknown <crismai.dev@gmail.com>
2023-10-11 16:49:52 -0300

    Testando a conexão com o banco de dados

---
+++ src/test/db/dbConfig.test.js
@@ -0,0 +1,21 @@
+import { describe, expect } from '@jest/globals';
+import db from '../../db/dbconfig';
+
+describe('Testando configDB', () => {
+    it('Teste de conexão com o banco de dados', async () => {
+        const autorMock = {
+            nome: "Cris",
+            nacionalidade: "Brasileiro",
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString()
+
+        };
+        const autorSalvo = await db('autores').insert(autorMock)
+            .then((retorno) => db('autores')
+            .where('id', retorno[0]))
+            .then((autorSelecionado) => autorSelecionado[0]);
+            
+        expect(autorSalvo.nome).toBe(autorMock.nome);
+    });
+  });
+  
